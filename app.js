0b4fbea510f66ab9904466d7f75c11b73f166b2f
fvirani786 <67715327+fvirani786@users.noreply.github.com>
2023-02-04 10:59:35 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// Questions will be asked
+const Questions = [{
+    id: 0,
+    q: "What is capital of Texas",
+    a: [{ text: "Dallas", isCorrect: false },
+        { text: "Houston", isCorrect: false },
+        { text: "Austin", isCorrect: true },
+        { text: "Carrollton", isCorrect: false }
+    ]
+
+},
+{
+    
+}
+
+
